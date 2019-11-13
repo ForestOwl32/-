63532174ceeb746abb0d8302b8e229961db3c602
ForestOwl32 <54009594+ForestOwl32@users.noreply.github.com>
2019-11-13 23:17:32 +0900

    Update 빅스비.py

--- 빅스비.py
+++ 빅스비.py
@@ -1,1 +1,17 @@
-s
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("빅스비 활성화")
+  await client.change_presence(game=discord.Game(name='', type=1))
+
+@client.event
+async def on_message(message):
+  if message.content.startswith("hi"):
+    await client.send_message(message.channel, "HI")
+    
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
