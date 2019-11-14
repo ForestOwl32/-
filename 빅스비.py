ae6f7a4b6a19883ed9d0fccfabf894af30b40594
ForestOwl32 <54009594+ForestOwl32@users.noreply.github.com>
2019-11-14 16:19:42 +0900

    Update 빅스비.py

--- 빅스비.py
+++ 빅스비.py
@@ -7,7 +7,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print("빅스비 활성화")
-  game = discord.Game("빅스비 개발 중..")
+  game = discord.Game("\"빅스비 도움\" 입력 ㄱㄱ")
   await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
