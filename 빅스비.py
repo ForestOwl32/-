304589618854df5f4e43fd00ff49864fce3e0a77
ForestOwl32 <54009594+ForestOwl32@users.noreply.github.com>
2019-11-16 00:42:50 +0900

    Update 빅스비.py

--- 빅스비.py
+++ 빅스비.py
@@ -14,12 +14,18 @@ async def on_message(message):
   if message.content.startswith("하이 빅스비"):
     await message.channel.send("안녕하세요!")
   elif message.content.startswith("안녕 빅스비"):
-    await message.channel.send("안녕하세요"):
+    await message.channel.send("안녕하세요!")
     
+  if message.content.startswith("/공지사항"):
+    channel = message.conetnt[7:25]
+    msg = message.content[26:]
+    await client.get_channel(int(channel)).send(msg)
+
+
   if message.content.startswith("빅스비"):
     await message.channel.send("네, 플레이어님")
     
-  if message.conetnt.startswith("빅스비 바보"):
+  if message.conetnt.startswith("빅스비는 바보"):
     await message.channel.send("플레이어님이 더 바보")
     
   if message.content.startswith("빅스비 모두불러줘"):
